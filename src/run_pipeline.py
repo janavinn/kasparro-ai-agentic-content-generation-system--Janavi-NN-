928cc1250da4f9564fb048c337ba38f4161c2487
Janavi n n <163169179+janavinn@users.noreply.github.com>
2025-12-25 01:00:37 +0530

    Add main function to run pipeline with Orchestrator

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,1 +1,8 @@
+from src.orchestrator import Orchestrator
 
+def main():
+    orch = Orchestrator(input_path="src/input/product.json", out_dir="outputs")
+    orch.run()
+
+if __name__ == "__main__":
+    main()
